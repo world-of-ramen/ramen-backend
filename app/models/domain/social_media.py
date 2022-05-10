29013b749b3e6de4afdf8cb9010a002c8cab710e
Alex1114 <pokai.eed05@g2.nctu.edu.tw>
2022-05-10 13:17:37 +0800

    add website field

--- app/models/domain/social_media.py
+++ app/models/domain/social_media.py
@@ -6,3 +6,4 @@ from app.models.domain.ramenmodel import RamenModel
 class SocialMedia(RamenModel):
     facebook: Optional[str] = None
     instagram: Optional[str] = None
+    website: Optional[str] = None
